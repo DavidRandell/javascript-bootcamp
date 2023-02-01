6861a99d8dde40fbbc0ad5b8bea1b8d079f0a1d2
DavidRandell <david@davidrandell.com>
2023-02-01 14:50:30 +0000

    Finished Section

--- todo-app/js/todo-app.js
+++ todo-app/js/todo-app.js
@@ -1,55 +1,11 @@
-const todos = [{
-	text: 'Buy Groceries',
-	completed: false
-}, {
-	text: 'Gas up the Car',
-	completed: false
-}, {
-	text: 'Pick up the kids',
-	completed: true
-}, {
-	text: 'Hit the gym',
-	completed: false
-}, {
-	text: 'Eat Lunch',
-	completed: true
-}]
+// Main Todo App JS file
 
-// 1. Setup a div to contain the Todos list
-// 2. Setup filters (searchText) and wire up a new filter input to change it
-// 3. Create a renderTodos function to render and rerender the latest filtered Data
-
-// STARTS
+const todos = getSavedTodos()
 
 // Search/Filter input text object 
 const filters = {
-	searchText: ''
-}
-
-const renderTodos = function(todos, filters) {
-	//filter todos based on the search value
-	const filteredTodos = todos.filter(function(todo){
-		return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-	})
-	// Incomplete Todos
-	const incompleteTodos = filteredTodos.filter(function(todo){
-		return !todo.completed
-	})
-
-	// Clear Todos before rerendering
-	document.querySelector('#todos').innerHTML = ''
-
-	const summary = document.createElement('h3')
-	summary.textContent = `You have ${incompleteTodos.length} todos left`
-	document.querySelector('#todos').appendChild(summary)
-
-	// Render All Todos
-	filteredTodos.forEach(function(todo){
-		const p = document.createElement('p')
-		p.className = 'todo'
-		p.textContent = todo.text
-		document.querySelector('#todos').appendChild(p)
-	})
+	searchText: '',
+	hideCompleted: false
 }
 
 renderTodos(todos, filters)
@@ -60,18 +16,24 @@ document.querySelector('#filter').addEventListener('input', function(e){
 	renderTodos(todos, filters)
 })
 
-
-
-
-
 // Functionality Stuff
 
-document.querySelector('#addTodo').addEventListener('click', function(){
-	console.log('Adding a new Todo!')
+document.querySelector('#new-todo').addEventListener('submit', function(e){
+	e.preventDefault()
+	todos.push({
+		text: e.target.elements.text.value,
+		completed: false
+	})
+	saveTodos(todos)
+	renderTodos(todos, filters)
+	e.target.elements.text.value = ''
 })
 
-// Listen for new todos text change
 
-document.querySelector('#new-todo-text').addEventListener('input', function(e){
- console.log(e.target.value)
+// Hide Completed functionality
+document.querySelector('#hide-completed').addEventListener('change', function(e){
+	filters.hideCompleted = e.target.checked
+	renderTodos(todos, filters)
 })
+
+
